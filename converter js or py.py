9830a330293e2c0fbaaa5e62d92a4e97accb5c54
UazeH <89394300+UazeH@users.noreply.github.com>
2022-03-08 09:00:17 +0700

    Update converter js or py.py

--- converter js or py.py
+++ converter js or py.py
@@ -1,64 +1,77 @@
 import os, sys, time
 
-os.system("color 0b")
-os.system("title github.com/uazeh")
-print("=================\nConvert JS/Py to Exe\ngithub.com/uazeh\nUaze Ganteng:V\n=================")
-pilih = input("[1] JS to Exe\n[2] Py to Exe\n#--> = ")
-if pilih == "1":
-    os.system("npm install -g pkg")
+def listmenu():
+    os.system("title Menu List")
+    os.system("color 0b")
+    os.system('cls')
+    print("""[!] Github : github.com/uazeh
+
+[+] Menu :
+
+[1] JS TO EXE
+[2] PY TO EXE
+[3] Install pkg (if you already installed it you don't need to install it again)
+[4] Install pyinstaller (if you already installed it you don't need to install it again)
+""")
+
+    pilih = input("[>] Select Menu: ")
+
+    if pilih == "1":jstoexe()
+    elif pilih == '2':pytoexe()
+    elif pilih == '3':installpkg()
+    elif pilih == '4':pyinstalls()
+    else:
+        os.system("cls")
+        print("Menu Not Available!")
+        print(" ")
+        y = input("Back To List Menu? (y/n): ")
+    if y == "y":
+        listmenu()
+
+def jstoexe():
     os.system("cls")
     os.system("color 0b")
-    os.system("title JS to Exe")
-    print("=================")
-    print("Convert JS to Exe")
-    print("github.com/uazeh")
-    print("Uaze Ganteng:V")
-    print("=================")
+    os.system("title JS TO EXE")
     namefile = input("Enter name file (Must 1 directory) : ")
     time.sleep(1)
     os.system("pkg " + namefile + ".js")
     time.sleep(1)
-    os.system("color 0b")
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-elif pilih == '2':
-    os.system("pip install pyinstaller")
     os.system("cls")
     os.system("color 0b")
-    os.system("title Py to Exe")
-    print("=================")
-    print("Convert Py to Exe")
-    print("github.com/uazeh")
-    print("Uaze Ganteng:V")
-    print("=================")
+    print("File already converted...")
+    y = input("Back To List Menu? (y/n): ")
+    if y == "y":
+        listmenu()
+
+def pytoexe():
+    os.system("cls")
+    os.system("color 0b")
+    os.system("title PY TO EXE")
     namefile1 = input("Enter name file (Must 1 directory) : ")
     time.sleep(1)
     os.system("pyinstaller -F " + namefile1 + ".py")
-    time.sleep(1)
+    time.sleep(5)
+    os.system("cls")
     os.system("color 0b")
     print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
-    print("File already converted...")
-    time.sleep(5)
+    y = input("Back To List Menu? (y/n): ")
+    if y == "y":
+        listmenu()
+
+def installpkg():
+    os.system("npm install -g pkg")
+    os.system("cls")
+    os.system("color 0b")
+    y = input("Back To List Menu? (y/n): ")
+    if y == "y":
+        listmenu()
+
+def pyinstalls():
+    os.system("pip install pyinstaller")
+    os.system("cls")
+    os.system("color 0b")
+    y = input("Back To List Menu? (y/n): ")
+    if y == "y":
+        listmenu()
+
+listmenu()
